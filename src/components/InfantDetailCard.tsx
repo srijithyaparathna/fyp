d1b2592f6ba80942dcef86e1f5e5145366b0198f
SKYjithyapa <125755221+SKYjithyapa@users.noreply.github.com>
2025-08-12 14:27:35 +0530

    infant part done

--- src/components/InfantDetailCard.tsx
+++ src/components/InfantDetailCard.tsx
@@ -3,6 +3,10 @@ import { Box, Typography, Avatar, Button, Tabs, Tab } from '@mui/material';
 import VitalsCard from './VitalsCard';
 import HistoryCard from './HistoryCard';
 import LiveFeedCard from './LiveFeedCard';
+import Grid from '@mui/material/Grid';
+import Divider from '@mui/material/Divider';
+import StatusIndicator from './StatusIndicator';
+import ChartCard from './ChartCard';
 
 export type Infant = {
   id: string;
@@ -38,30 +42,153 @@ const InfantDetailCard = ({ infant, onBack, subView, setSubView }: Props) => {
 
   // Default Profile view
   return (
+    <Box>
     <Box
-      sx={{
-        p: 3,
-        backgroundColor: 'white',
-        borderRadius: 4,
-        boxShadow: 2,
-      }}
+     p={4} bgcolor="white" borderRadius={15} boxShadow={2} 
     >
-      <Button variant="outlined" onClick={onBack} sx={{ mb: 2 }}>
+      <Button variant="outlined" onClick={onBack} sx={{ mb: 3 }}>
         ← Back to List
       </Button>
 
-      <Box display="flex" alignItems="center" gap={2} mb={2}>
-        <Avatar src={infant.image} sx={{ width: 60, height: 60 }} />
-        <Typography variant="h5">{infant.id}</Typography>
-      </Box>
+      {/* Top profile section  */}
+      <Grid container spacing={3} alignItems='center' columnSpacing={4} >
+   <Grid item xs={12} md={3} sx={{ml:10}}  >
+  <Box display="flex" alignItems="center" flexDirection="column">
+    <Avatar src={infant.image} sx={{ width: 100, height: 100, mb: 4 }} />
+    <Typography variant="h6">{infant.id}</Typography>
+    <Button
+  variant="outlined"
+  size="small"
+  sx={{
+    mt: 4,
+    borderRadius: '15px', // You can adjust the px value
+    borderColor: 'black',
+    color: 'black',
+    '&:hover': {
+      borderColor: 'black', // Keeps it black on hover
+      backgroundColor: '#f0f0f0', // Optional hover background
+    },
+  }}
+>
+  🎥 Video Call
+</Button>
 
-      <Typography>Incubator: {infant.incubator}</Typography>
+  </Box>
+  </Grid>
+
+
+  {/* Vertical Divider */}
+  <Divider orientation="vertical" flexItem  sx={{ml:10}} />
+
+
+
+
+      <Grid item xs={12} md={9} >
+
+      <Grid sx={{ml:10}} >
+
+        <Grid container spacing={20} >
+          <Grid item xs={6} >
+            <Box display="flex" flexDirection="column">
+              <Typography>Sex:</Typography>
+              <br></br>
+              <Typography><b>Male</b></Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={6} >
+            <Box display="flex" flexDirection="column">
+              <Typography>Date of Birth:</Typography>
+              <br></br>
+              <Typography><b>05/04/2025</b></Typography>
+            </Box>
+
+          </Grid>
+          <Grid item xs={6} >
+            <Box display="flex" flexDirection="column">
+              <Typography>Age:</Typography>
+              <br></br>
+              <Typography><b>10 Days</b></Typography>
+            </Box>
+
+          </Grid>
+          <Grid item xs={6} >
+            <Box display="flex" flexDirection="column">
+              <Typography>Birth Weigh:</Typography>
+              <br></br>
+              <Typography><b>700g</b></Typography>
+            </Box>
+
+          </Grid>
+          <Grid item xs={6} >
+            <Box display="flex" flexDirection="column">
+              <Typography>Blood:</Typography>
+              <br></br>
+              <Typography><b>A+</b></Typography>
+            </Box>
+
+          </Grid>
+          <Grid item xs={6} >
+            <Box display="flex" flexDirection="column">
+              <Typography>Incubator</Typography>
+              <br></br>
+              <Typography><b>Male</b></Typography>
+            </Box>
+
+          </Grid>
+
+        </Grid>
+      </Grid>
+
+  {/* Parent/Guardian details */}
+
+     <Box mt={2} sx={{mt:10,ml:10}} >
+            <Typography variant="body2" fontWeight="bold">
+              Parent / Guardian Details
+            </Typography>
+            <br></br>
+            <Grid >
+              <Grid display={'flex'} gap={8} >
+                      <Typography variant="body2">
+              Mother's Name: Ms. Amali G.S
+
+            </Typography>
+            |
+            <Typography variant='body2' >
+              Contact Number : 0714356782
+            </Typography>
+              </Grid>
+
+<br></br>
+                        <Grid display={'flex'} gap={8} >
+                      <Typography variant="body2">
+              Mother's Name: Ms. Amali G.S
+
+            </Typography>
+            |
+            <Typography variant='body2' >
+              Contact Number : 0714356782
+            </Typography>
+              </Grid>
+
+
+         
+            </Grid>
+           
+          
+          </Box>
+
+
+
+
+</Grid>
+</Grid>
+
+     {/* <Typography>Incubator: {infant.incubator}</Typography>
       <Typography>Jaundice Status: {infant.jaundiceStatus}</Typography>
       <Typography>Crying Status: {infant.cryingStatus}</Typography>
       <Typography>Video: {infant.video}</Typography>
       <Typography>Doctor: {infant.doctor}</Typography>
-      <Typography>Last Update: {infant.lastUpdate}</Typography>
-
+      <Typography>Last Update: {infant.lastUpdate}</Typography> */}
       {/* Tabs */}
       <Box mt={4}>
         <Tabs
@@ -76,6 +203,53 @@ const InfantDetailCard = ({ infant, onBack, subView, setSubView }: Props) => {
         </Tabs>
       </Box>
     </Box>
+
+ {/* Charts + Status Section */}
+<Box mt={4}>
+  <Grid container spacing={3}>
+    {/* Chart 1 */}
+    <Grid item xs={12} md={6}>
+      <ChartCard title="Temperature Chart">
+        {/* chart content goes here */}
+      </ChartCard>
+    </Grid>
+
+    {/* Chart 2 */}
+    <Grid item xs={12} md={6}>
+      <ChartCard title="Humidity Chart"
+      title = "Humidity Chart"
+      type = "humidity"
+        apiUrl="https://api.open-meteo.com/v1/forecast?latitude=51.5074&longitude=-0.1278&hourly=relativehumidity_2m"
+      >
+        {/* chart content goes here */}
+      </ChartCard>
+    </Grid>
+
+  </Grid>
+</Box>
+
+<Box mt={4} >
+  <Grid container spacing={3} >
+    {/* Chart 3 */}
+    <Grid item xs={12} md={6}>
+      <ChartCard title="ΔT Measurement">
+        {/* chart content goes here */}
+      </ChartCard>
+    </Grid>
+
+ <Grid item xs={12} md={6} display="flex" justifyContent="center" alignItems="center">
+      <StatusIndicator
+        label1="Crying Status"
+        status1={infant.cryingStatus}
+        label2="Jaundice Status"
+        status2={infant.jaundiceStatus}
+      />
+    </Grid>
+
+   </Grid>
+</Box>
+
+    </Box> 
   );
 };
 
